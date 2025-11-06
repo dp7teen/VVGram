8962d7c8a32a34fb44b0a8e39e5ebb2794171eaa
Deviprasath <deviprasath@screen-magic.com>
2025-11-06 13:41:01 +0530

    Corrected Post apis

--- src/main/java/com/dp/vvgram/controllers/PostController.java
+++ src/main/java/com/dp/vvgram/controllers/PostController.java
@@ -8,9 +8,11 @@ import com.dp.vvgram.exceptions.PostingServiceNotAvailableException;
 import com.dp.vvgram.exceptions.UserNotFoundException;
 import com.dp.vvgram.models.Post;
 import com.dp.vvgram.services.PostService;
+import com.dp.vvgram.utilities.PrincipalHelper;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,42 +26,35 @@ public class    PostController {
         this.postService = postService;
     }
 
-    @PostMapping("/{username}")
-    public ResponseEntity<String> uploadPost(@PathVariable String username,
-                                             @RequestBody PostRequestDto requestDto) throws UserNotFoundException, PostingServiceNotAvailableException {
-        String message = postService.upload(username, requestDto);
+    @PostMapping
+    public ResponseEntity<String> uploadPost(@RequestBody PostRequestDto requestDto) throws UserNotFoundException, PostingServiceNotAvailableException {
+        UserDetails details = PrincipalHelper.getPrincipal();
+        String message = postService.upload(details.getUsername(), requestDto);
         return new ResponseEntity<>(message, HttpStatus.CREATED);
     }
 
-    @PatchMapping("/edit/{username}/{postId}")
-    public ResponseEntity<String> updatePost(@PathVariable String username,
-                                             @PathVariable Long postId,
+    @PatchMapping("/{postId}")
+    public ResponseEntity<String> updatePost(@PathVariable Long postId,
                                              @RequestBody PostRequestDto requestDto) throws UserNotFoundException, PostNotFoundException {
+        UserDetails details = PrincipalHelper.getPrincipal();
         return new ResponseEntity<>(
-                postService.editPost(username, postId, requestDto),
+                postService.editPost(details.getUsername(), postId, requestDto),
                 HttpStatus.CREATED
         );
     }
 
-    @DeleteMapping("/delete/{username}/{id}")
-    public ResponseEntity<String> deletePost(@PathVariable String username,
-                                             @PathVariable Long id) throws UserNotFoundException, PostNotFoundException {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable Long id) throws UserNotFoundException, PostNotFoundException {
+        UserDetails details = PrincipalHelper.getPrincipal();
         return new ResponseEntity<>(
-                postService.delete(username, id),
+                postService.delete(details.getUsername(), id),
                 HttpStatus.OK
         );
     }
 
-    @PostMapping
+    @GetMapping
     public Page<PostDto> getPostsByUser(@RequestBody GetPostsDto getPostsDto) throws UserNotFoundException {
         return postService.getPostsByUser(getPostsDto.getUsername(),
                 getPostsDto.getPageno(), getPostsDto.getPagesize(), getPostsDto.getSortBy());
-    }
-
-    @GetMapping("/{username}/{id}")
-    public PostDto getPostById(@PathVariable String username,
-                               @PathVariable long id) throws UserNotFoundException, PostNotFoundException {
-        Post post = postService.getPostById(username, id);
-        return PostDto.from(List.of(post)).getFirst();
     }
 }
